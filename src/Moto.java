eb2b3b2c778685a470e0c9525cbe36c19f999f51
EAbeier <emersonabeier@hotmail.com>
2020-05-25 20:12:38 -0300

    Melhorada a classe que extende classe Veiculo

--- src/Moto.java
+++ src/Moto.java
@@ -1,9 +1,19 @@
 import java.util.Scanner;
 
-public class Moto extends Veiculo{
+public class Moto extends Veiculo {
+	private double custoInicial = 1.00;
+	private double custoAdicional = 1.50;
 
+	public Moto(String placa, String tipoVeiculo) {
+		super(placa, tipoVeiculo);
+	}
 
-	public Moto(){
-			String tipoVeiculo = "Motocicleta";
+	public double getCustoInicial() {
+
+		return custoInicial;
+	}
+
+	public double getCustoAdicional() {
+		return custoAdicional;
 	}
 }
