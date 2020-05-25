0aa14a01e69d466a52d6b2ec58420f8fd4e9ceb2
EAbeier <emersonabeier@hotmail.com>
2020-05-25 16:37:45 -0300

    incrementado o controlador com inputs

--- src/Controlador.java
+++ src/Controlador.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Controlador {
 
@@ -19,11 +20,60 @@ public class Controlador {
     public void pagar(){
 
     }
-    public void iniciaEstacionamento(){
+    public void consultaEstacionamento(){
 
     }
+    public void consultaFluxo(){
+
+    }
+    public void iniciaEstacionamento() {
+        Scanner scn = new Scanner(System.in);
+        int escolha = 0;
+        while (escolha != 99) {
+            System.out.println("---------------------------\n"+
+                                "    ESTACIONAMENTO POO\n"+
+                                "---------------------------\n" +
+                                "Escolha a opção desejada:\n"+
+                                " 1 - Cadastrar entrada de Veículo\n"+
+                                " 2 - Cadastrar saida de Veículo\n"+
+                                " 3 - gerar pagamento\n"+
+                                " 4 - relatorio de veiculos estacionados\n"+
+                                " 5 - entradas e saidas do dia\n"+
+                                " 99 - SAIR\n");
+            escolha = scn.nextInt();
+            switch (escolha) {
+                case 1: {
+                    estaciona();
+                    break;
+                }
+                case 2: {
+                    saiEstacionamento();
+                    break;
+                }
+                case 3: {
+                    pagar();
+                    break;
+                }
+                case 4: {
+                    consultaEstacionamento();
+                    break;
+                }
+                case 5: {
+                    consultaFluxo();
+                    break;
+                }
+                case 99: {
+                    break;
+                }
+                default: {
+                    System.out.println("escolha não cadastrada.");
+                    break;
+
+                }
+
+            }
+        }
 
 
-
-
+    }
 }
