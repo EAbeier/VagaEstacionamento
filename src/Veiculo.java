e0e95f6ab059eceb0bb55126c0d7df320fb2475e
EAbeier <emersonabeier@hotmail.com>
2020-05-25 20:11:55 -0300

    Melhorada a classe abstrata

--- src/Veiculo.java
+++ src/Veiculo.java
@@ -1,7 +1,29 @@
-public abstract class Veiculo{
-		private String tipoVeiculo;
+import java.util.Date;
 
+public abstract class Veiculo {
+	private String tipoVeiculo;
+	private int hora;
+	private String placa;
 
+	public Veiculo(String placa, String tipoVeiculo) {
+		this.tipoVeiculo = tipoVeiculo;
+		this.placa = placa;
 
+		Date date = new Date();
+		this.hora = date.getHours();
 
+	}
+	public String getTipo(){
+		return tipoVeiculo;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+	public abstract double getCustoInicial();
+	public abstract double getCustoAdicional();
 }
+
+
+
+
