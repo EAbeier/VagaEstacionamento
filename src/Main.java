00dc7e8a906b0a46dbd2bec5c8a08c917241169f
EAbeier <emersonabeier@hotmail.com>
2020-05-26 19:34:29 -0300

    ajustada a identação.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
-public class Main {
-    public static void main (String[] args){
+public class Main
+{
+    public static void main (String[] args)
+    {
         Controlador controler = new Controlador();
         controler.iniciaEstacionamento();
     }
