14549eaededbd74a903e675b2619d042d116652a
EAbeier <emersonabeier@hotmail.com>
2020-05-26 19:29:41 -0300

    adiconado metodo getHora

--- src/Caminhonete.java
+++ src/Caminhonete.java
@@ -1,19 +1,22 @@
 import java.util.Scanner;
 
-public class Caminhonete extends Veiculo{
+public class Caminhonete extends Veiculo
+{
 	private double custoInicial = 3.00;
 	private double custoAdicional = 3.50;
 
-	public Caminhonete(String placa,Tipo tipoVeiculo ){
+	public Caminhonete(String placa,Tipo tipoVeiculo )
+	{
 		super(placa, tipoVeiculo);
 	}
 
-	public double getCustoInicial() {
-
+	public double getCustoInicial()
+	{
 		return custoInicial;
 	}
 
-	public double getCustoAdicional() {
+	public double getCustoAdicional()
+	{
 		return custoAdicional;
 	}
 }
