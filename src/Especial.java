72c92cf18ed5c32ae3f89140ce28298f40f6811b
EAbeier <emersonabeier@hotmail.com>
2020-05-26 19:32:58 -0300

    ajustada a identação.

--- src/Especial.java
+++ src/Especial.java
@@ -1,10 +1,9 @@
-public class Especial extends Vaga {
-    public Especial(){
+public class Especial extends Vaga
+{
+    public Especial()
+    {
         tipoVaga = "especial";
     }
 
-    @Override
-    public void setStatus (){
 
-    }
 }
